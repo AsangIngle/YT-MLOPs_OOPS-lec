6cdfe3bffc601a2d4baea935e251ac056032a7fc
Asang Triratna Ingle <asangingle07@gmail.com>
2025-06-22 18:54:43 +0530

    signin added

--- OOPS_proj.py
+++ OOPS_proj.py
@@ -13,9 +13,9 @@ class chatbook:
                          4.press 4 to message  a friend,
                          5.press any key to exit""")
         if user_input=='1':
-            pass
+            self.signup()
         elif user_input=='2':
-            pass
+            self.signin()
         elif user_input=='3':
             pass
         elif user_input=='4':
@@ -23,4 +23,25 @@ class chatbook:
         else:
             exit()
             
+            
+    def signup(self):
+        email=input("enter your email here: ")
+        pass_word=input("set password: ")
+        self.username=email
+        self.password=pass_word
+        print("you have signed up succesfully")
+        print('\n')
+        self.menu()
+        
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("signup first: ")
+        else:
+            username=input("enter your username")
+            pass_word=input("enter your password")
+            if self.username==username and self.password==pass_word:
+                print("you have signed in succesfully! ")
+                self.loggedin=True
+            else:
+                print("please input correct crediantials >>")
 onj=chatbook()
