44127cbc98449bacfd70273b9cd6344a907c75a4
Asang Triratna Ingle <asangingle07@gmail.com>
2025-06-22 19:09:57 +0530

    done

--- OOPS_proj.py
+++ OOPS_proj.py
@@ -1,47 +1,70 @@
 class chatbook:
     def __init__(self):
-        self.username=''
-        self.password=''
-        self.loggedin=False
+        self.username = ''
+        self.password = ''
+        self.loggedin = False
+        self.posts = []
+        self.messages = []
         self.menu()
-        
+
     def menu(self):
-        user_input=input("""welcome to chatbook ! how would you like to procid
-                         1.press 1 to signup,
-                         2.press 2 to signin,
-                         3.press 3 to writ a post,
-                         4.press 4 to message  a friend,
-                         5.press any key to exit""")
-        if user_input=='1':
+        user_input = input("""Welcome to Chatbook! How would you like to proceed?
+1. Press 1 to Sign Up
+2. Press 2 to Sign In
+3. Press 3 to Write a Post
+4. Press 4 to Message a Friend
+5. Press any other key to Exit
+Enter your choice: """)
+        if user_input == '1':
             self.signup()
-        elif user_input=='2':
+        elif user_input == '2':
             self.signin()
-        elif user_input=='3':
-            pass
-        elif user_input=='4':
-            pass
+        elif user_input == '3':
+            self.write_post()
+        elif user_input == '4':
+            self.message_friend()
         else:
+            print("Exiting... Goodbye!")
             exit()
-            
-            
+
     def signup(self):
-        email=input("enter your email here: ")
-        pass_word=input("set password: ")
-        self.username=email
-        self.password=pass_word
-        print("you have signed up succesfully")
-        print('\n')
+        email = input("Enter your email: ")
+        pass_word = input("Set your password: ")
+        self.username = email
+        self.password = pass_word
+        print("You have signed up successfully.\n")
         self.menu()
-        
+
     def signin(self):
-        if self.username=='' and self.password=='':
-            print("signup first: ")
+        if self.username == '' and self.password == '':
+            print("Signup first.\n")
         else:
-            username=input("enter your username")
-            pass_word=input("enter your password")
-            if self.username==username and self.password==pass_word:
-                print("you have signed in succesfully! ")
-                self.loggedin=True
+            username = input("Enter your username: ")
+            pass_word = input("Enter your password: ")
+            if self.username == username and self.password == pass_word:
+                print("You have signed in successfully!\n")
+                self.loggedin = True
             else:
-                print("please input correct crediantials >>")
-onj=chatbook()
+                print("Please input correct credentials.\n")
+        self.menu()
+
+    def write_post(self):
+        if self.loggedin:
+            post = input("Write your post: ")
+            self.posts.append(post)
+            print("Post submitted!\n")
+        else:
+            print("You must be signed in to write a post.\n")
+        self.menu()
+
+    def message_friend(self):
+        if self.loggedin:
+            friend = input("Enter your friend's name: ")
+            message = input("Enter your message: ")
+            self.messages.append((friend, message))
+            print("Message sent!\n")
+        else:
+            print("You must be signed in to send messages.\n")
+        self.menu()
+
+onj = chatbook()
